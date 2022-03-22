d460cf133eaa82535acc8cbd23ed2ff175dc56ce
Taebah shlouh <119taeba@gmail.com>
2022-03-23 01:43:06 +0300

    add

--- monday21-3-2022/task2.js
+++ monday21-3-2022/task2.js
@@ -1,1 +1,11 @@
 "use strict"
+//////task1/////////////////////
+
+function tellFortune(x,y,z,n) {
+    return("You will be a "+ x +"in " + y +", and married to" + z + " with " + n +" kids.");
+}
+   console.log(tellFortune('software engineer', 'Jordan', 'Alice', 3)); 
+///////task2//////////////////////
+   function calculateDogAge (params) {
+       
+   } 
