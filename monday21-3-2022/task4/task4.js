72d0b689b40ce64b88153cb05e0b48f2056b2619
Taebah shlouh <119taeba@gmail.com>
2022-03-23 08:59:05 +0300

    stars pattern

---
+++ monday21-3-2022/task4/task4.js
@@ -0,0 +1,11 @@
+"use strict"
+let n = 5;
+let str = "";
+for (let i = 1; i <= n; i++) {////rows/
+  for (let j = 0; j < i; j++) { ////columns//
+    str += " * ";
+  }
+  str += "\n";
+}
+console.log(str);
+document.writeln(str);
