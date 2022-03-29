1499b1192a5f8220c3b378bad6fb0bdfc0b5afe4
Taebah shlouh <119taeba@gmail.com>
2022-03-29 21:17:20 +0300

    task4

--- wednesday23-3-2022/task4/app.js
+++ wednesday23-3-2022/task4/app.js
@@ -1,1 +1,44 @@
 "use strict";
+// Create a simple what-you-see-what-you-get text editor.
+//   - Change the text inside the "text" div according to the form selections.
+
+function fontFamily() {
+    let f_m =document.getElementById("fontFa").value;
+    if(f_m=="Courier New"){
+          document.getElementById("textar").style.fontFamily="Courier New";
+    }
+    else if(f_m=="Lucida Sans"){
+           document.getElementById("textar").style.fontFamily="Lucida Sans";
+    }
+    else if(f_m=="Arial"){
+        document.getElementById("textar").style.fontFamily="Arial";
+    }
+}
+function fontSize(){
+    let f_s =document.getElementById("fS").value;
+    if (f_s=="12px"){
+        document.getElementById("textar").style.fontSize="12px";
+    }else if(f_s=="18px"){
+        document.getElementById("textar").style.fontSize="18px";
+    }else if(f_s=="22px"){
+        document.getElementById("textar").style.fontSize="22px";
+    }
+}
+function fStyle() {
+    let text_are = document.getElementById("textar");
+    let italic = document.getElementById("Italic").checked;
+    text_are.style.fontStyle = (italic ? "italic" : "normal");
+}
+
+
+function fWeight() {
+    let text_are = document.getElementById("textar");
+    let bo = document.getElementById("Bold").checked;
+    text_are.style.fontWeight = (bo ? "bold" : "normal");
+}
+
+function tDecoration(){
+    let text_are = document.getElementById("textar");
+    let unde_line = document.getElementById("Underline").checked;
+    text_are.style.textDecorationLine = (unde_line ? "underline" : "");
+}
--- wednesday23-3-2022/task5/app.js
+++ wednesday23-3-2022/task5/app.js
@@ -1,1 +1,7 @@
 "use strict";
+// - Create Html page with two password fields and a submit button.
+// - Perform input check and display an error message next the corresponding field if there is an error.
+//   * Passwords must be at least 6 characters long.
+//   * The two passwords must match.
+// - The placeholders for the error messages have already been prepared (div-s with class "err").
+// - By default the submit button should be hidden. Display the button only when there are no errors.
