937ff2ed1c3b78b331b080fffe13910f4a31a01b
Taebah shlouh <119taeba@gmail.com>
2022-03-29 16:33:09 +0300

    ;

---
+++ tuesday29-3-2022/task2/app.js
@@ -0,0 +1,49 @@
+"use strict";
+let locform = document.getElementById("mobForm");
+let pargloc =  document.getElementById("cityResult")
+
+
+
+
+function getvalue(loccation)
+{
+     this.loccation = loccation;
+     
+     outputlocation(loccation);
+
+}
+
+async function outputlocation(x)
+{
+    let respone = await fetch(`https://eu1.locationiq.com/v1/search.php?key=pk.146180d54ef1b81b7922cf956cba402d&q=${x}&format=json`)
+    let data = await respone.json()
+   
+   
+   let createname = document.createElement("span");
+   pargloc.appendChild(createname)
+   createname.innerHTML= `Location Name: ${data[0].display_name} <br>
+   lot: ${data[0].lat}
+   lon: ${data[0].lon}
+   <br>`
+   let imgsrc = data[0].icon
+   console.log(imgsrc);
+   document.getElementById("imgg").src = `${imgsrc}`
+    
+    console.log(data);
+
+
+}
+
+locform.addEventListener("submit" , getfromform ) 
+function getfromform(event) 
+{
+    event.preventDefault();
+
+    let loccation = event.target.locName.value ; 
+    console.log(loccation);
+    new getvalue(loccation)
+
+
+
+}
+// getfromform();
