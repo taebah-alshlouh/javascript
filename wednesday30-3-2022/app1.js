92e43fbdd93f5b5e27cf67e39d6d9af3914ed9b4
Taebah shlouh <119taeba@gmail.com>
2022-03-31 09:04:21 +0300

    add

--- wednesday30-3-2022/app1.js
+++ wednesday30-3-2022/app1.js
@@ -3,14 +3,14 @@ let show=document.getElementById('show');
 
 function GetDataFromLStorage(){
     let Data=localStorage.getItem('formData');
-    let parseData=JSON.parse(Data);
-    console.log(parseData);
-    renderData(parseData)
+    let conData=JSON.parse(Data);
+    console.log(conData);
+    renderData(conData)
 }
 GetDataFromLStorage()
 
-function renderData(parseData){
-    parseData.map(function(i){
+function renderData(conData){
+    conData.map(function(i){
     result.innerHTML= `I'm  ${i.fullName},  my birth date is ${i.bdate}`;
     })
 }
