0de0d355b8164b69d0c14d56014cb06530267366
Taebah shlouh <119taeba@gmail.com>
2022-03-31 07:27:54 +0300

    check the password validation

--- wednesday23-3-2022/task5/app.js
+++ wednesday23-3-2022/task5/app.js
@@ -5,3 +5,30 @@
 //   * The two passwords must match.
 // - The placeholders for the error messages have already been prepared (div-s with class "err").
 // - By default the submit button should be hidden. Display the button only when there are no errors.
+
+let subm =document.getElementById("sub");
+subm.style.visibility = "hidden";
+
+function checkValidation(){
+   let firstPass = document.getElementById("pass");
+   let repeatPass = document.getElementById("re_pass");
+   let message1 = document.getElementById("check1");
+   let message2 = document.getElementById("check2");
+
+               
+   if(firstPass.value.length >= 6){
+        message1.innerHTML = "Correct : the length is 6";
+   }
+   else{
+        message1.innerHTML="wrong :Length is short";
+   }
+       
+   if (firstPass.value == repeatPass.value) {
+       message2.innerHTML= "Password Match";
+       subm.style.visibility="visible";
+   }
+   else if(firstPass.value != repeatPass.value ){
+       message2.innerHTML=" Password Doesn't match";
+   }
+
+}
