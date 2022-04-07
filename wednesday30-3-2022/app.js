1e2f7814413dc07a9eb0a096302a221141ba69a8
Taebah shlouh <119taeba@gmail.com>
2022-04-07 13:05:29 +0300

    last

--- wednesday30-3-2022/app.js
+++ wednesday30-3-2022/app.js
@@ -1,9 +1,74 @@
 "use strict";
-let reForm=document.getElementsByName("regestForm");
+let regestForm=document.getElementsByName("regestForm");
 let button=document.getElementById("sub");
+let fName=document.getElementById('f_name');
+let lName=document.getElementById('L_name');
+let birthDate=document.getElementById('bdate');
+let email=document.getElementById('email');
+let password=document.getElementById('password');
+
 let data=[];
 
-button.addEventListener('click',submithandler);
+
+function storeInLocal(){
+    let array=JSON.stringify(data);
+    localStorage.setItem('Data',array);
+}
+
+
+
+
+// function validation(fName,lName,birthDate,email,email2,password,repass) {
+//     let pwd_expression = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+//     let letters = /[A-Za-z]+$/;
+//     let filter = /([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// ////////////////////////////////
+//     if(fName =='')
+//            alert("Enter your Name");
+//      else if( fName.test(letters)===fName.value)
+//              return true;
+//      else
+//          alert("The name is not valid");
+// ///////////////////////////////////////
+//      if(lName =='')
+//         alert("Enter your Name");
+//      else if(lName.test(letters)===lName.value)
+//             return true;
+//     else
+//             alert("The name is not valid");
+// /////////////////////////////////////////////
+
+//     if (password.match(pwd_expression)) 
+
+//         alert("Valid email address!");
+//      else 
+//         alert("Invalid email address!");
+    
+// ////////////////////////////////////
+//    if(email.test(filter)===email)
+//     alert("valid Email");
+//     else
+//     alert("invalid Email")
+// //////////////////////////////////
+//     if (email2=='') 
+//        { alert("enter your email again");}
+    
+//     else if(email2.test(filter))
+//        {return true}
+    
+//     else
+//       { alert("the email invalid");}
+// ///////////////////////////////////
+//        if(email=== email2){
+//            return email2;
+
+//        }
+// ////////////////////////////////////
+   
+
+
+
+regestForm.addEventListener('submit',submithandler);
 function submithandler(event) {
     event.preventDefault();
     let fName=event.target.f_name.value;
@@ -13,88 +78,10 @@ function submithandler(event) {
     let email2=event.target.email2.value;
     let password=event.target.password.value;
     let repass=event.target.password2.value;
-   data.push(fName,lName,birthDate)
+   data.push(fName,lName,birthDate);
  
-   console.log(regesForm);
    validation(fName,lName,birthDate,email,email2,password,repass)
-   LocalStorageFrom()
-}
-
-
-function LocalStorageFrom(){
-    let array=JSON.stringify(info);
-    localStorage.setItem('Data',array);
-    if(array!='')
-    return true;
+   storeInLocal()
 }
 
 submithandler();
-
-
-
-function validation(fName,lName,birthDate,email,email2,password,repass) {
-    let pwd_expression = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
-    let letters = /[A-Za-z]+$/;
-    let filter = /([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-////////////////////////////////
-    if(fName =='')
-           alert("Enter your Name");
-     else if( fName.test(letters)===fName.value)
-             return true;
-     else
-         alert("The name is not valid");
-///////////////////////////////////////
-     if(lName =='')
-        alert("Enter your Name");
-     else if(lName.test(letters)===lName.value)
-            return true;
-    else
-            alert("The name is not valid");
-/////////////////////////////////////////////
-    if(birthDate == '')
-      alert("Enter your birth date");
-    else if(birthDate.test(filter)===birthDate.value)
-           return true;
-    else
-    alert("invalid date");
-///////////////////////////////////////
-    if (password.match(pwd_expression)) 
-
-        alert("Valid email address!");
-     else 
-        alert("Invalid email address!");
-    
-////////////////////////////////////
-   if(email.test(filter)===email)
-    alert("valid Email");
-    else
-    alert("invalid Email")
-//////////////////////////////////
-    if (email2=='') 
-       { alert("enter your email again");}
-    
-    else if(email2.test(filter))
-       {return true}
-    
-    else
-      { alert("the email invalid");}
-///////////////////////////////////
-       if(email=== email2){
-           return email2;
-
-       }
-////////////////////////////////////
-   
-
-    }//////////////funn end/////////
-function checkDate(date){
- let rege=/(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
- if(rege.test(date)){
-     console.log('correct date');
-     return date;
- }
- else{
-     console.log('inncorrect date');
-     emailError.innerHTML='inncorrect date';
- }
-}
